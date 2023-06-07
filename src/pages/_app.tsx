935fca13dc315c4d3ac4773a72f9626e5226b5a7
stangatimu <stangatimu@gmail.com>
2023-06-07 21:31:35 +0300

    feat: dark theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,50 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { ConfigProvider, theme } from "antd";
 import { store } from "@/store";
 import { Provider } from "react-redux";
+import { Col, Row, Switch } from "antd";
+
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+  };
+  
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <style jsx global>{`
+        html {
+          background: ${isDarkMode ? "black" : "white"};
+        }
+      `}</style>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        }}
+      >
+        <Row
+          justify={"center"}
+          style={{
+            marginTop: "10px",
+          }}
+        >
+          <Col lg={14} md={18} sm={22} xs={23}>
+            <Row justify={"end"}>
+              <Switch
+                unCheckedChildren="Light"
+                checkedChildren="Dark"
+                onChange={handleClick}
+              />
+            </Row>
+          </Col>
+        </Row>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </Provider>
   );
 }
